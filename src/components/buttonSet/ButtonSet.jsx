bbfcfc126d1befa9e1cf75b16690e0163391d608
Azam Fahmy <azamf@infyinnos.com>
2022-07-17 18:26:11 +0530

    add styling to manage student form

--- src/components/buttonSet/ButtonSet.jsx
+++ src/components/buttonSet/ButtonSet.jsx
@@ -1,11 +1,13 @@
 import { Button } from "reactstrap";
 
-const ButtonSet = ({onSave, onDelete, onReset}) => {
+const ButtonSet = ({ onSave, onDelete, onReset }) => {
   return (
-    <div>
-      {onSave && <Button onClick={onSave} color="success">Save </Button>}
-      {onDelete && <Button onClick={onDelete} color="danger">Delete</Button>}
-      {onReset && <Button onClick={onReset} color="warning">Reset</Button>}
+    <div class="container">
+      <div class="row justify-content-end">
+        {onSave && <div class="col-1"><Button onClick={onSave} color="success">Save </Button></div>}
+        {onDelete && <div class="col-1"> <Button onClick={onDelete} color="danger">Delete</Button></div>}
+        {onReset && <div class="col-1"> <Button onClick={onReset} color="warning">Reset</Button></div>}
+      </div>
     </div>
   );
 };
--- src/pages/manage/students/ManageStudents.jsx
+++ src/pages/manage/students/ManageStudents.jsx
@@ -3,7 +3,7 @@ import CustomForm from "../../../components/customForm/CustomForm";
 import NavigationBar from "../../../components/navbar/NavigationBar";
 import { FormGroup, Label, Input } from "reactstrap";
 import { useState } from "react";
-import {DateTime} from "luxon";
+import { DateTime } from "luxon";
 
 const ManageStudents = () => {
   const [student, setStudent] = useState({});
@@ -15,11 +15,11 @@ const ManageStudents = () => {
   };
 
   const sampleData = [
-    {id: "1", name: "john", age: 45, wives: 2 },
-    {id: "2", name: "sam", age: 35, wives: 12 },
+    { id: "1", name: "john", age: 45, wives: 2 },
+    { id: "2", name: "sam", age: 35, wives: 12 },
   ];
 
-  const classroomData = [{id:1,name:'class 1'},{id:2,name:'class 2'}]
+  const classroomData = [{ id: 1, name: 'class 1' }, { id: 2, name: 'class 2' }]
 
   const onSave = () => {
     console.log("saving...", student);
@@ -31,88 +31,116 @@ const ManageStudents = () => {
 
   const onReset = () => setStudent({});
 
-  const onEdit = (e) => console.log("refreshing page for...",e.target.id);
+  const onEdit = (e) => console.log("refreshing page for...", e.target.id);
 
   console.log(student);
   return (
     <>
       <NavigationBar />
-      <CustomForm title="Manage Students" onDelete={onDelete} onReset={onReset} onSave={onSave}>
-        <FormGroup>
-          <Label for="firstName">First Name</Label>
-          <Input
-            id="firstName"
-            type="text"
-            value={student.firstName || ""}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="lastName">Last Name</Label>
-          <Input
-            id="lastName"
-            type="text"
-            value={student.lastName || ""}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="contactPerson">Contact Person</Label>
-          <Input
-            id="contactPerson"
-            type="text"
-            value={student.contactPerson || ""}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="contactNo">Contact No.</Label>
-          <Input
-            id="contactNo"
-            type="text"
-            value={student.contactNo || ""}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="email">Email Address</Label>
-          <Input
-            id="email"
-            type="email"
-            value={student.email || ""}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="dob">Date of Birth</Label>
-          <Input
-            id="dob"
-            type="date"
-            value={student.dob || ""}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="age">Age</Label>
-          <Input
-            id="age"
-            readOnly
-            value={student.dob && DateTime.now().diff(DateTime.fromFormat(student.dob, 'yyyy-MM-dd')).toFormat("yy") || "-"}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="classroom">Classroom</Label>
-          <Input
-            id="classroom"
-            type="select"
-            value={student.classroom || ""}
-            onChange={handleChange}
-          >
-          {classroomData && classroomData.map(classroom => <option key={classroom.id}>{classroom.name}</option> )}
-          </Input>
-        </FormGroup>
-      </CustomForm>
-      <CustomTable title="Existing Students" table={sampleData} buttonHandler={onEdit} buttonText="Edit" />
+      <div class="container p-3">
+        <CustomForm title="Manage Students" onDelete={onDelete} onReset={onReset} onSave={onSave}>
+          <div class="row">
+            <div class="col">
+              <FormGroup>
+                <Label for="firstName">First Name</Label>
+                <Input
+                  id="firstName"
+                  type="text"
+                  value={student.firstName || ""}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </div>
+            <div class="col">
+              <FormGroup>
+                <Label for="lastName">Last Name</Label>
+                <Input
+                  id="lastName"
+                  type="text"
+                  value={student.lastName || ""}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <FormGroup>
+                <Label for="contactPerson">Contact Person</Label>
+                <Input
+                  id="contactPerson"
+                  type="text"
+                  value={student.contactPerson || ""}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </div>
+            <div class="col">
+              <FormGroup>
+                <Label for="contactNo">Contact No.</Label>
+                <Input
+                  id="contactNo"
+                  type="text"
+                  value={student.contactNo || ""}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <FormGroup>
+                <Label for="email">Email Address</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  value={student.email || ""}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </div>
+            <div class="col">
+              <FormGroup>
+                <Label for="dob">Date of Birth</Label>
+                <Input
+                  id="dob"
+                  type="date"
+                  value={student.dob || ""}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <FormGroup>
+                <Label for="age">Age</Label>
+                <Input
+                  id="age"
+                  readOnly
+                  value={student.dob && DateTime.now().diff(DateTime.fromFormat(student.dob, 'yyyy-MM-dd')).toFormat("yy") || "-"}
+                />
+              </FormGroup>
+            </div>
+            <div class="col">
+              <FormGroup>
+                <Label for="classroom">Classroom</Label>
+                <Input
+                  id="classroom"
+                  type="select"
+                  value={student.classroom || ""}
+                  onChange={handleChange}
+                >
+                  {classroomData && classroomData.map(classroom => <option key={classroom.id}>{classroom.name}</option>)}
+                </Input>
+              </FormGroup>
+            </div>
+          </div>
+        </CustomForm>
+      </div>
+      <div class="container p-3">
+        <CustomTable title="Existing Students" table={sampleData} buttonHandler={onEdit} buttonText="Edit" />
+      </div>
     </>
   );
 };
